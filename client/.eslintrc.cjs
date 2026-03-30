4ebf3768eaaeb254be799400502350aa6aa479bf
pankajyadav2006 <pankaj.yadav@adypu.edu.in>
2026-03-30 18:37:03 +0530

    fix

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,12 +1,19 @@
 module.exports = {
     root: true,
-    env: { browser: true, es2020: true },
+    env: { browser: true, es2020: true, node: true },
     extends: [
         'eslint:recommended',
         'plugin:react/recommended',
         'plugin:react/jsx-runtime',
         'plugin:react-hooks/recommended',
     ],
+    globals: {
+        vi: true,
+        describe: true,
+        it: true,
+        expect: true,
+        jest: true,
+    },
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
     settings: { react: { version: '18.2' } },
